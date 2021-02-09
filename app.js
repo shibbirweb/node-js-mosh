2bf702ab4ab20cbb3c4891ad2694b3777165fd3d
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-02-10 01:40:20 +0600

    05. Loading a Module

---
+++ app.js
@@ -0,0 +1,3 @@
+const log = require('./logger');
+
+log('message');
--- logger.js
+++ logger.js
@@ -5,5 +5,4 @@ function log(message){
     console.log(message)
 }
 
-module.exports.log = log;
-module.exports.endPoint = url;
+module.exports = log;
