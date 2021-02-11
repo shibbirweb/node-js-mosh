52c366d895e258639ba8eb0ff0437355f4e37050
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-02-11 23:33:54 +0600

    11. Event Arguments

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on('messageLogged', () => {
-    console.log('Listener called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
 });
 
 // Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', { id: 1, url: 'http://'});
 
 
-
+// Raise: Logging (data: message)
