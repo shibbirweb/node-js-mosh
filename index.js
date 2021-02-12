bf400c30ca7f56c5a044a1554efd8fe43e4e1abc
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-02-12 20:47:27 +0600

    09. Handling POST request

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "course 1" },
   { id: 2, name: "course 2" },
@@ -14,6 +16,18 @@ app.get("/", (req, res) => {
 
 app.get("/api/courses", (req, res) => {
   res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+
+    res.send(course);
+
 });
 
 app.get("/api/courses/:id", (req, res) => {
