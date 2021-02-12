5aef2d2f85de2109e44f1c5e3f76a43aa5fdf3d5
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-02-13 00:35:12 +0600

    04. Built in middleware

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const logger = require('./logger')
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.use(logger);
 
 app.use(function(req, res, next) {
   console.log('Authenticating...');
+  next();
 });
 
 const courses = [
