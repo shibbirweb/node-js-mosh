61e4671d764d64c56e69d066e4e6b70b8ca1fe4b
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-02-12 20:24:10 +0600

    06. Environment variable

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+//PORT
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
