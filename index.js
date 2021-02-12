2559d1587840fd28f9b3d3590c5d7fa5c32c86a8
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-02-12 20:29:02 +0600

    07. Route Parameter

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.get("/api/post/:year/:month", (req, res) => {
+  res.send(req.query);
 });
 
 //PORT
