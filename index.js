8392cfe52cd98e566632b706cd0d7783743a7c3c
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-02-12 20:41:51 +0600

    08. Handling Get request

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require("express");
 
 const app = express();
 
+const courses = [
+  { id: 1, name: "course 1" },
+  { id: 2, name: "course 2" },
+  { id: 3, name: "course 3" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.get("/api/post/:year/:month", (req, res) => {
-  res.send(req.query);
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found.");
+  else res.send(course);
 });
 
 //PORT
