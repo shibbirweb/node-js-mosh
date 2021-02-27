0df88f920c77f32a106e3a3feb30cc6bb2c9c499
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-02-28 00:54:20 +0600

    3- Callbacks

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 console.log("Before");
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+  console.log(user);
+
+  // get the repositories
+  getRepositories(user.gitHubUsername, (repositories) => {
+      console.log(repositories);
+  });
+});
 console.log("After");
 
-// Callback
-// Promises
-// Async/await
-
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from database...");
-    return {
-        id: id,
-        gitHubUsername: 'shibbir'
-    }
+    callback({
+      id: id,
+      gitHubUsername: "shibbir",
+    });
   }, 2000);
+}
+
+function getRepositories(username, callback){
+    setTimeout(() => {
+        console.log('Calling GitHub API...');
+        const repositories = ['repo1', 'repo2', 'repo3'];
+        callback(repositories);
+    }, 2000);
 }
