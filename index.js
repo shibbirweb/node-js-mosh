40922bcad53cc4e36d9cd32675e950049b0c3b42
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-03-03 00:51:23 +0600

    09- Comparison Query Operators

--- index.js
+++ index.js
@@ -31,11 +31,22 @@ async function createCourse(){
 }
 
 async function getCourses(){
+
+    // Comparison operator
+    // eq (equal)
+    // ne (not equal)
+    // ge (greater than)
+    // gte (greater than or equal to)
+    // lt (less than)
+    // lte (less than or equal to)
+    // in
+    // nin (not in)
+
+
     const courses = await Course
-    .find({
-        author: 'Mosh',
-        isPublished: true
-    })
+    //.find({ author: 'Mosh', isPublished: true })
+    //.find({ price: { $gte: 10, $lte: 20 } })
+    .find({ price: { $in: [10, 15, 20]} })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1});
