c676c134728df67b33a646d1d3cedeecf66c5cb8
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-03-03 00:45:01 +0600

    08- Querying Documents

--- index.js
+++ index.js
@@ -30,4 +30,17 @@ async function createCourse(){
     console.log(result);
 }
 
-createCourse();
+async function getCourses(){
+    const courses = await Course
+    .find({
+        author: 'Mosh',
+        isPublished: true
+    })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1});
+
+    console.log(courses);
+}
+
+getCourses();
