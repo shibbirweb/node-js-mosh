8173f2690ebfb13b7cd65e10052795ec7def093b
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-03-03 00:28:08 +0600

    05- Schemas

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
 .then(() => console.log('Connected to MongoDB...'))
 .catch(error => console.log('Could not connect to MongoDB...', error));
+
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
