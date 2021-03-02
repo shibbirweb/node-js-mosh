14d0ac9a2108d200b3dea69abace1985713cb9b4
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2021-03-03 00:55:24 +0600

    10- Logical Query Operators

--- index.js
+++ index.js
@@ -1,57 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-.then(() => console.log('Connected to MongoDB...'))
-.catch(error => console.log('Could not connect to MongoDB...', error));
-
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((error) => console.log("Could not connect to MongoDB...", error));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [ String ],
-    date: { type: Date, default: Date.now },
-    isPublished: Boolean
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
 });
 
 // Class, objects
 // Course, nodeCourse
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
-async function createCourse(){
-    const course = new Course({
-        name: 'Angular Course',
-        author: 'Mosh',
-        tags: ['angular', 'frontend'],
-        isPublished: true
-    });
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "Mosh",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
 
-    const result = await course.save();
-    console.log(result);
+  const result = await course.save();
+  console.log(result);
 }
 
-async function getCourses(){
+async function getCourses() {
+  // Comparison operator
+  // eq (equal)
+  // ne (not equal)
+  // ge (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  // lte (less than or equal to)
+  // in
+  // nin (not in)
 
-    // Comparison operator
-    // eq (equal)
-    // ne (not equal)
-    // ge (greater than)
-    // gte (greater than or equal to)
-    // lt (less than)
-    // lte (less than or equal to)
-    // in
-    // nin (not in)
+  // Logical Operator
+  // or
+  // and
 
-
-    const courses = await Course
-    //.find({ author: 'Mosh', isPublished: true })
-    //.find({ price: { $gte: 10, $lte: 20 } })
-    .find({ price: { $in: [10, 15, 20]} })
+  const courses = await Course
+    // .find({ author: 'Mosh', isPublished: true })
+    // .find({ price: { $gte: 10, $lte: 20 } })
+    // .find({ price: { $in: [10, 15, 20]} })
+    .find()
+    .or([{ author: "Mosh" }, { isPublished: true }])
+    .and()
     .limit(10)
     .sort({ name: 1 })
-    .select({ name: 1, tags: 1});
+    .select({ name: 1, tags: 1 });
 
-    console.log(courses);
+  console.log(courses);
 }
 
 getCourses();
