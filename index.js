51e7a9f0b092eed8d7270f832bcaafb60d6d76a6
Shibbir Ahmed <shibbirweb@gmail.com>
2021-03-04 22:29:25 +0600

    11- Regular Expressions

--- index.js
+++ index.js
@@ -49,9 +49,19 @@ async function getCourses() {
     // .find({ author: 'Mosh', isPublished: true })
     // .find({ price: { $gte: 10, $lte: 20 } })
     // .find({ price: { $in: [10, 15, 20]} })
-    .find()
-    .or([{ author: "Mosh" }, { isPublished: true }])
-    .and()
+    // .find()
+    // .or([{ author: "Mosh" }, { isPublished: true }])
+    // .and()
+
+    // starts with Mosh
+    .find({ author: /^Mosh/ })
+
+    // Ends with Hamedani
+    .find({ author: /Hamdedani$/i })
+
+    // Contains Mosh
+    .find({ author: /.*Mosh.*/i })
+
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
